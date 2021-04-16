f8ba46f7621bab929e0b9c2e6c76160eee1369b2
Shavin Croos <t.s.croos@student.reading.ac.uk>
2021-04-16 20:11:48 +0100

    Added onTouchEvent method
    Added onTouchEvent method in GamePanel.java class.

--- SpaceInvaders/app/src/main/java/com/shavin/spaceinvaders/GamePanel.java
+++ SpaceInvaders/app/src/main/java/com/shavin/spaceinvaders/GamePanel.java
@@ -1,6 +1,7 @@
 package com.shavin.spaceinvaders;
 
 import android.content.Context;
+import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
 
@@ -9,16 +10,24 @@ import androidx.annotation.NonNull;
 // Implements this interface to receive information about changes to the surface.
 public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
-    /*
-    GamePanel allows access to application-specific resources and classes,
-    as well as up-calls for application-level operations such as launching activities,
-    broadcasting abd receiving intents, etc.
+    /**
+     *  GamePanel allows access to application-specific resources and classes,
+     *  as well as up-calls for application-level operations such as launching activities,
+     *  broadcasting and receiving intents, etc.
+     * @param c
      */
+
     public GamePanel(Context c) {
 
+        /**
+         * Context tells the current state of the application or object.
+         * Allows for instantiated objects to grasp what events have taken
+         * place. This means that information about another section of the program
+         * can be called.
+         */
         super(c);
 
-        // getHolder() intercepts events.
+        // getHolder() has a callback method which intercepts events.
         getHolder().addCallback(this);
 
         // setFocusable makes GamePanel focus on handling events.
@@ -26,18 +35,45 @@ public class GamePanel extends SurfaceView implements SurfaceHolder.Callback {
 
     }
 
+    /**
+     * Called immediately after surface is first instantiated. Implementations
+     * of this should start up whatever rendering code chosen.
+     * @param holder
+     */
     @Override
     public void surfaceCreated(@NonNull SurfaceHolder holder) {
 
     }
 
+    /**
+     * Called after any structural changes (format or size) have been made to the surface.
+     * @param holder
+     * @param format
+     * @param width
+     * @param height
+     */
     @Override
     public void surfaceChanged(@NonNull SurfaceHolder holder, int format, int width, int height) {
 
     }
 
+    /**
+     * Called immediately before surface is destroyed. Access to this surface will not be allowed
+     * after call is returned.
+     * @param holder
+     */
     @Override
     public void surfaceDestroyed(@NonNull SurfaceHolder holder) {
 
+    }
+
+    /**
+     * onTouchEvent allows a check to see if user touches the screen or not
+     * @param event
+     * @return
+     */
+    @Override
+    public boolean onTouchEvent(MotionEvent event){
+        return super.onTouchEvent(event);
     }
 }
--- SpaceInvaders/app/src/main/java/com/shavin/spaceinvaders/MainActivity.java
+++ SpaceInvaders/app/src/main/java/com/shavin/spaceinvaders/MainActivity.java
@@ -1,7 +1,6 @@
 package com.shavin.spaceinvaders;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.view.Window;
 import android.view.WindowManager;
@@ -12,9 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
+        // Removes title.
         requestWindowFeature(Window.FEATURE_NO_TITLE);
 
+        // Makes sure that the app is properly set to full screen.
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+
 
         setContentView(new GamePanel(this));
     }
